6be1457a6d9e17f1690611412636d40f866d6ce8
ItsAlex <me@itsalex.fr>
2017-11-14 20:24:07 +0100

    Fix #3

--- libft/srcs/ft_error.c
+++ libft/srcs/ft_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malexand <malexand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 17:01:17 by malexand          #+#    #+#             */
-/*   Updated: 2017/02/24 16:24:22 by malexand         ###   ########.fr       */
+/*   Updated: 2017/11/14 20:23:21 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	error(int error, int send_perror, char *str)
 {
 	if (send_perror == 1)
 	{
-		ft_putstr_color("Error \x1B[0m: ", RED);
+		ft_putstr_fd("Error \x1B[0m: ", -1);
 		perror("");
 	}
 	else
 	{
-		ft_putstr_color("Error", RED);
-		ft_putstr(" : ");
-		ft_putendl(str);
+		ft_putstr_fd("Error", -1);
+		ft_putstr_fd(" : ", -1);
+		ft_putendl_fd(str, -1);
 	}
 	exit(error);
 }
