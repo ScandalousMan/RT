5011dd29c5d8706d996b99236e514c5fe03c62a6
adrien <aguemy@student.42.fr>
2018-08-06 00:55:41 +0200

    end 06Aug stereo ongoing

--- srcs/constructor.c
+++ srcs/constructor.c
@@ -45,34 +45,68 @@ t_param			*pxl_infos_create(t_param *param)
 	return (param);
 }
 
+t_sdl				*copy_pxls(t_param *param)
+{
+	t_sdl *graph;
+	int 	count;
+
+	count = -1;
+	if (!(graph = (t_sdl*)malloc(sizeof(t_sdl))))
+		return (NULL);
+	ft_bzero((void*)graph->input, sizeof(char) * SDL_NUM_SCANCODES);
+	while (++count < SDL_NUM_SCANCODES)
+		graph->input[count] = FALSE;
+	graph->input[count - 1] = '\0';
+	graph->win_gl = param->graph->win_gl;
+	graph->gl_context = param->graph->gl_context;
+	graph->win_sdl = param->graph->win_sdl;
+	graph->render_sdl = param->graph->render_sdl;
+	count = 0;
+	while (count < NB_THREAD)
+	{
+		if ((graph->surfs[count] = SDL_CreateRGBSurfaceWithFormat(0,
+		WINDOW_SDL_WIDTH, WINDOW_SDL_HEIGHT / NB_THREAD, 32, SDL_PIXELFORMAT_ARGB8888))
+		== NULL)
+			error(0, 0, "Can't create all surface");
+		if ((graph->tmp_surfs[count] = SDL_CreateRGBSurfaceWithFormat(0,
+		WINDOW_SDL_WIDTH, WINDOW_SDL_HEIGHT / NB_THREAD, 32, SDL_PIXELFORMAT_ARGB8888))
+		== NULL)
+			error(0, 0, "Can't create all surface");
+		count++;
+	}
+	graph->show_tmp = 1;
+	return graph;
+}
+
 t_param			*stereoscopy_cpy(t_param *param1)
 {
-	t_param		*param;
+	t_param	*param;
 
 	if (!(param = (t_param*)malloc(sizeof(t_param))))
 		return (NULL);
-
 	param->f = param1->f;
-	pt_translated(param1->eye, param1->align, param->eye);
+	vec_multiply(10.0, param1->align, param1->tmp_vec);
+	pt_translated(param1->eye, param1->tmp_vec, param->eye);
+	vec_multiply(-1.0, param1->align, param->align);
 	vec_copy(param1->look, param->look);
-	vec_copy(param1->align, param->align);
-	vec_copy(param1->third, param->third);
+	vec_to_unit_norm(vector_product(param->align, param->look, param->third));
 	param->path = param1->path;
 	param->customs = param1->customs;
 	param->objects = param1->objects;
 	param->lights = param1->lights;
 	param->num_lights = param1->num_lights;
-	param->intersect_object = param1->intersect_object;
-	param->tmp_light = param1->tmp_light;
+	param->intersect_object = NULL;
+	param->tmp_light = NULL;
 	param->brightness = param1->brightness;
 	param->epsilon = param1->epsilon;
-	param->graph = param1->graph;
 	param->thread = param1->thread;
-	param->current_thread = param1->current_thread;
+	param->current_thread = 0;
 	param->refresh = param1->refresh;
 	param->ia = param1->ia;
 	param->macro = param1->macro;
 	param->pxl_infos = param1->pxl_infos;
+	if (!(param->graph = copy_pxls(param1)))
+		return (NULL);
 	return (param);
 }
 
@@ -89,7 +123,6 @@ t_param			*struct_create(void)
 	param->customs = NULL;
 	param->objects = NULL;
 	param->lights = NULL;
-//	param->tmp_object = NULL;
 	param->tmp_light = NULL;
 	param->intersect_object = NULL;
 	param->f = FOCAL_VALUE;
