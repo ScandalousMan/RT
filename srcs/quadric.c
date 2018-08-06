52a922a5c216275628b11ca792cc2def3cea2bc7
adrien <aguemy@student.42.fr>
2018-08-06 17:13:11 +0200

    quadric init from 23

---
+++ srcs/quadric.c
@@ -0,0 +1,16 @@
+#include <rt.h>
+
+double	distance_to_quadric(t_object *tmp, double *from, double *to)
+{
+
+}
+
+void	update_normal_quadric(t_object *tmp, t_path *path)
+{
+
+}
+
+int		is_inside_quadric(t_object *tmp, t_path *path)
+{
+
+}
---
+++ srcs/quadric_tools.c
@@ -0,0 +1,16 @@
+#include "rt.h"
+
+double	quadric_first_term(t_object *tmp, double *to)
+{
+
+}
+
+double	quadric_second_term(t_object *tmp, double *to, double *x)
+{
+
+}
+
+double	quadric_third_term(t_object *tmp, double *x)
+{
+
+}
